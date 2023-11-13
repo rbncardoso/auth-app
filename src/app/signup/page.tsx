c12fed7a2ba8d22c8f385ac73a8d512e312661d5
Ruben M Cardoso <rubenmcardoso22@gmail.com>
2023-11-13 11:50:30 -0100

    modify .env file

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 export default function SignUpPage() {
   return (
     <div className='flex justify-center'>
-    <div className=''>
-    <h1 className=' text-center text-white text-2xl'>Signup</h1>
+      <div className=''>
+        <h1 className=' text-center text-white text-2xl'>Signup</h1>
 
+      </div>
     </div>
-  </div>
   )
 }
