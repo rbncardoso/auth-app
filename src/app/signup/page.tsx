b9c91a56797f78b3faebe10206f95b48f8be1eec
Ruben M Cardoso <rubenmcardoso22@gmail.com>
2023-11-13 23:59:01 -0100

    added signup inputs and button

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,12 +1,62 @@
+"use client";
+import Link from "next/link";
 import React from 'react'
+import { useRouter } from "next/navigation";
+import { Axios } from "axios";
 
 export default function SignUpPage() {
-  return (
-    <div className='flex justify-center'>
-      <div className=''>
-        <h1 className=' text-center text-white text-2xl'>Signup</h1>
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+  });
 
-      </div>
+  const onSignup =async () => {
+    
+  }
+
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+      <h1>Signup</h1>
+      <hr />
+      <label htmlFor="username">username</label>
+      <input 
+      className="p-2 border border-gray-300 rounded-lg
+      mb-4 focus:outline-none focus:border-gray-600 text-black"
+      id="username"
+      type="text" 
+      value={user.username}
+      onChange={(e)=> setUser({...user, username: e.target.value})}
+      placeholder="username"
+      />
+      <label htmlFor="email">email</label>
+      <input 
+      className="p-2 border border-gray-300 rounded-lg
+      mb-4 focus:outline-none focus:border-gray-600 text-black"
+      id="email"
+      type="text"
+      value={user.email}
+      onChange={(e)=> setUser({...user, email: e.target.value})} 
+      placeholder="email"
+      />
+      <label htmlFor="password">password</label>
+      <input 
+      className="p-2 border border-gray-300 rounded-lg
+      mb-4 focus:outline-none focus:border-gray-600 text-black"
+      id="password"
+      type="password" 
+      value={user.password}
+      onChange={(e)=>setUser({...user, password: e.target.value})}
+      placeholder="password"
+      />
+      <button 
+       onClick={onSignup}
+       className="p-2 border border-gray-300 rounded-lg
+       mb-4 focus:outline-none focus:border-gray-600 hover:shadow-white"
+      >
+        Signup here
+      </button>
+      
     </div>
   )
 }
