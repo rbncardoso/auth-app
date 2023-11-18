074b47bd27300ecdc91b092f47efb560e28c1b31
Ruben M Cardoso <rubenmcardoso22@gmail.com>
2023-11-18 00:48:31 -0100

    error on authentication, data not goig to db

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,7 +2,8 @@
 import Link from "next/link";
 import React, { use, useEffect } from 'react'
 import { useRouter } from "next/navigation";
-import  axios  from "axios";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 export default function SignUpPage() {
   const router = useRouter();
@@ -13,22 +14,35 @@ export default function SignUpPage() {
   });
 
   const [buttonDisable, setButtonDisabled] = React.useState(false);
-
+  const [loading, setLoading] = React.useState(false);
+  
   const onSignup = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/signup", user);
+      console.log("Signup success", response.data);
+      router.push("/login");
+    } catch (error: any) {
+      console.log("Signup failed", error.message)
+
+      toast.error(error.message)
+    } finally {
+      setLoading(false)
+    }
 
   }
 
-  useEffect(()=>{
-    if(user.email.length > 0 && user.username.length > 0 && user.password.length > 0){
+  useEffect(() => {
+    if (user.email.length > 0 && user.username.length > 0 && user.password.length > 0) {
       setButtonDisabled(false)
-    } else{
+    } else {
       setButtonDisabled(true)
     }
-  },[user])
+  }, [user])
 
   return (
     <div className='flex flex-col items-center justify-center min-h-screen py-2'>
-      <h1>Signup</h1>
+      <h1>{loading ? "Processing" : "Signup"}</h1>
       <hr />
       <label htmlFor="username">username</label>
       <input
@@ -65,7 +79,7 @@ export default function SignUpPage() {
         className="p-2 border border-gray-300 rounded-lg
        mb-4 focus:outline-none focus:border-gray-600 hover:shadow-white"
       >
-        {buttonDisable? "No signup" : "Signup"}
+        {buttonDisable ? "No signup" : "Signup"}
       </button>
       <Link href="/login">Visit Login page</Link>
 
