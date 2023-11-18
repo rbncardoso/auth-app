1bcebc422efcbb00c68a86d2d50289104a9feb2e
Ruben M Cardoso <rubenmcardoso22@gmail.com>
2023-11-18 01:17:14 -0100

    error fixed, Mongo db now recieve the created user and when you click it process until create a new user on the db and go to login page

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -2,9 +2,11 @@ import { connect } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
+import { connection } from "mongoose";
 
 
 connect()
+
 
 export async function POST(request: NextRequest) {
   try {
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,4 @@
+"use cliet"
 import React from 'react'
 
 export default function ProfilePage() {
