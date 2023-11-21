5f47ad613d72ceb59fcb508a022f26ad36e3bfa0
Ruben M Cardoso <rubenmcardoso22@gmail.com>
2023-11-20 23:16:56 -0100

    error on login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,23 +3,39 @@ import React from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import axios from 'axios'
+import {toast} from 'react-hot-toast'
 
 
 
 export default function LoginPage() {
 
+  const router = useRouter();
   const [user, setUser] = React.useState({
     email: "",
     password: "",
   });
+  const [loading, setLoading] = React.useState(false);
 
   const onLogin = async () => {
 
+    try {
+      setLoading(true)
+      const response = await axios.post("/api/users/login", user)
+      console.log("login success", response.data)
+     // toast.success("Login success");
+      router.push("/profile")
+
+    } catch (error: any) {
+      console.log("Login failed", error.message)
+      toast.error(error.message)
+    } finally{
+      setLoading(false);
+    }
   }
 
   return (
     <div className='flex flex-col items-center justify-center min-h-screen py-2'>
-      <h1>Login</h1>
+      <h1>{loading? "Processing" : "Login"}</h1>
       <hr />
       <label htmlFor="email">email</label>
       <input
